c31a3a70487a42097913ea6388811d9aeb8ece35
Nabyne <144941710+Nabyne1337@users.noreply.github.com>
2024-07-01 18:58:42 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rentex
+{
+    public partial class Form1 : Form
+    {
+        private string password;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Подтверждено",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            Form2 form2 = new Form2(password);
+            form2.Show();
+        }
+
+        private void scoreLabel_Click(object sender, EventArgs e)
+        {
+            // Implement if needed
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Implement if needed
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox1.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
